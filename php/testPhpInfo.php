7c3199c3f192cd5b7f58f1a81f4e757ffdecb040
Raul Fernando Mendoza Huerta <raul.mendoza@softtekprojects.com>
2024-08-27 18:46:09 -0600

    moving to sufolder

--- php/testPhpInfo.php
+++
@@ -1,8 +0,0 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<META HTTP-EQUIV="EXPIRES" CONTENT="0">
-</head>
-<body>
-	Hello PHP World <br>	Apache version:<?phpecho $_SERVER['SERVER_SOFTWARE'];?>		Is php runing<?php phpinfo(); ?>
-	time:<? echo phpinfo(); ?>
-</body>
