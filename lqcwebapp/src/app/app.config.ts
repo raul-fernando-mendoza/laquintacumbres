bf7be82ce971fc6ec04cd33305e8d6b7abace0e9
Raul Mendoza <raul_fernando_mendoza@hotmail.com>
2024-08-29 18:46:08 -0600

    using gallery

--- lqcwebapp/src/app/app.config.ts
+++ lqcwebapp/src/app/app.config.ts
@@ -4,7 +4,19 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideAnimationsAsync(),
+    {
+      provide: GALLERY_CONFIG,
+      useValue: {
+        autoHeight: true,
+        imageSize: 'cover'
+      } as GalleryConfig
+    }    
+  ]
 };
--- lqcwebapp/src/app/home/home.component.ts
+++ lqcwebapp/src/app/home/home.component.ts
@@ -1,36 +1,51 @@
 import { Component } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatDividerModule} from '@angular/material/divider';
+import {GalleryModule, GalleryItem, ImageItem } from 'ng-gallery';
+import { map, Observable, shareReplay } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatGridListModule,MatDividerModule],
+  imports: [CommonModule, MatGridListModule,MatDividerModule,GalleryModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
   imageUrl="/assets/images/lqc1.jpg";
 
-  imagelst=[
-    "/assets/images/lqc10.jpeg",
-    "/assets/images/lqc11.jpeg",
-    "/assets/images/lqc12.jpeg",
-    "/assets/images/lqc13.jpeg",
-    "/assets/images/lqc14.jpeg",
-    "/assets/images/lqc15.jpeg",
-    "/assets/images/lqc16.jpeg",
-    "/assets/images/lqc17.jpeg",
-    "/assets/images/lqc18.jpeg",
-    "/assets/images/lqc19.jpeg",
-    "/assets/images/lqc21.jpeg",
-    "/assets/images/lqc22.jpeg",
-    "/assets/images/lqc23.jpeg",
-    "/assets/images/lqc24.jpeg",
-    "/assets/images/lqc25.jpeg",
-    "/assets/images/lqc26.jpeg",
-    "/assets/images/lqc27.jpeg",
-    "/assets/images/lqc28.jpeg",
-    "/assets/images/lqc19.jpeg"    
+  images: GalleryItem[] = [
+    new ImageItem({src: "/assets/images/lqc10.jpeg", thumb: "/assets/images/lqc10.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc11.jpeg", thumb: "/assets/images/lqc11.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc12.jpeg", thumb: "/assets/images/lqc12.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc13.jpeg", thumb: "/assets/images/lqc13.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc14.jpeg", thumb: "/assets/images/lqc14.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc15.jpeg", thumb: "/assets/images/lqc15.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc16.jpeg", thumb: "/assets/images/lqc16.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc17.jpeg", thumb: "/assets/images/lqc17.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc18.jpeg", thumb: "/assets/images/lqc18.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc19.jpeg", thumb: "/assets/images/lqc19.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc20.jpeg", thumb: "/assets/images/lqc20.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc21.jpeg", thumb: "/assets/images/lqc21.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc22.jpeg", thumb: "/assets/images/lqc22.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc23.jpeg", thumb: "/assets/images/lqc23.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc24.jpeg", thumb: "/assets/images/lqc24.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc25.jpeg", thumb: "/assets/images/lqc25.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc26.jpeg", thumb: "/assets/images/lqc26.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc27.jpeg", thumb: "/assets/images/lqc27.jpeg"}),
+    new ImageItem({src: "/assets/images/lqc28.jpeg", thumb: "/assets/images/lqc28.jpeg"})
   ]
+
+  constructor(private breakpointObserver: BreakpointObserver){
+
+  }
 }
